2349d086fac55bcf5fccfe4670baf4289ee0124a
Yuttana <yuttana76>
2018-05-07 23:19:21 +0700

    Update server.js connect to db.

--- server.js
+++ server.js
@@ -13,6 +13,22 @@ var db;
 
 console.log('MONGODB_URI>>' + process.env.MONGODB_URI);
 
-app.listen(8000, () => {
-    console.log('Server started!');
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
   });
+});
+
+// CONTACTS API ROUTES BELOW
