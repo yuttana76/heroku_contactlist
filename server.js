07cab72938dd05eee881b92e9b1b2185a170a24b
Yuttana <yuttana76>
2018-05-08 09:27:18 +0700

    Add API GET:id, PUT and DELETE

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ app.use(bodyParser.json());
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
-console.log('MONGODB_URI>>' + process.env.MONGODB_URI);
-
 // Connect to the database before starting the application server.
 mongodb.MongoClient.connect(process.env.MONGODB_URI , function (err, client) {
   if (err) {
@@ -59,6 +57,46 @@ app.get("/api/contacts", function(req, res) {
         handleError(res, err.message, "Failed to create new contact.");
       } else {
         res.status(201).json(doc.ops[0]);
+      }
+    });
+  });
+
+  /*  "/api/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
+
+app.get("/api/contacts/:id", function(req, res) {
+    db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to get contact");
+      } else {
+        res.status(200).json(doc);
+      }
+    });
+  });
+  
+  app.put("/api/contacts/:id", function(req, res) {
+    var updateDoc = req.body;
+    delete updateDoc._id;
+  
+    db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to update contact");
+      } else {
+        updateDoc._id = req.params.id;
+        res.status(200).json(updateDoc);
+      }
+    });
+  });
+  
+  app.delete("/api/contacts/:id", function(req, res) {
+    db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+      if (err) {
+        handleError(res, err.message, "Failed to delete contact");
+      } else {
+        res.status(200).json(req.params.id);
       }
     });
   });
