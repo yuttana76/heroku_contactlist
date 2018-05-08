4861ba28b025d705f24790df16afd1404de87028
Yuttana <yuttana76>
2018-05-08 09:07:51 +0700

    Add API GET/POST todos

--- server.js
+++ server.js
@@ -32,3 +32,33 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI , function (err, client) {
 });
 
 // CONTACTS API ROUTES BELOW
+/*  "/api/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
+
+app.get("/api/contacts", function(req, res) {
+    db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+      if (err) {
+        handleError(res, err.message, "Failed to get contacts.");
+      } else {
+        res.status(200).json(docs);
+      }
+    });
+  });
+  
+  app.post("/api/contacts", function(req, res) {
+    var newContact = req.body;
+  
+    if (!req.body.name) {
+      handleError(res, "Invalid user input", "Must provide a name.", 400);
+    }
+  
+    db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to create new contact.");
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
+  });
