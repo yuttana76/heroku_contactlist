c0cbaa8bb9bc4e63e253bc59957a1d9319e97666
Yuttana <yuttana76>
2018-05-08 12:06:52 +0700

    Update PUT API

--- server.js
+++ server.js
@@ -118,7 +118,10 @@ app.get("/api/contacts/:id", function(req, res) {
     //  );
 
 
-    db.collection(CONTACTS_COLLECTION).updateOne({id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+    db.collection(CONTACTS_COLLECTION).updateOne({id: new ObjectID(req.params.id)}, {
+                name: updateDoc.name,
+                email: updateDoc.email
+            }, function(err, doc) {
       if (err) {
         handleError(res, err.message, "Failed to update contact");
       } else {
